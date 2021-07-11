4c2a7bc1f18c283b54159046de95e33b72773e36
OlegRS <oleg.r.senkevich@gmail.com>
2021-07-11 21:06:40 +0100

    fix: Correct the axes (which were reversed)

--- scripts/2CW_graphical_analysis.py
+++ scripts/2CW_graphical_analysis.py
@@ -31,8 +31,8 @@ axs[0].set_xlim([-1, 1])
 axs[0].set_ylim([-1, 1])
 axs[0].axhline(0, linewidth=1, color='black')
 axs[0].axvline(0, linewidth=1, color='black')
-axs[0].plot(m2, M1, label=r'Equation_1: $\frac{\partial F}{\partial m_1} = 0$', linewidth=2)
-axs[0].plot(M2, m1, label=r'Equation_2: $\frac{\partial F}{\partial m_2} = 0$', linewidth=2)
+axs[0].plot(m1, M2, label=r'Equation_1: $\frac{\partial F}{\partial m_1} = 0$', linewidth=2)
+axs[0].plot(M1, m2, label=r'Equation_2: $\frac{\partial F}{\partial m_2} = 0$', linewidth=2)
 
 axs[0].set_title("$\gamma_1=$" + str(gamma1) + ", $\gamma_2=$" + str(gamma2) + "; $J_{11}=$"+str(J11)+", $J_{22}=$"+str(J22)+", $J_{12}=$"+str(J12)+"; $h_1=$"+str(h1)+", $h_2=$"+str(h2), fontsize=12, y=1.01)
 axs[0].grid()
@@ -43,8 +43,8 @@ gamma2 = .5
 J11 = 3
 J22 = 3
 J12 = -.42
-h1 = 0
-h2 = .2
+h1 = 0.2
+h2 = -0.05
 
 M1 = np.arange(-1, 1,.00001)
 M2 = np.arange(-1, 1,.00001)
@@ -58,10 +58,10 @@ axs[1].set_xlim([-1, 1])
 axs[1].set_ylim([-1, 1])
 axs[1].axhline(0, linewidth=1, color='black')
 axs[1].axvline(0, linewidth=1, color='black')
-axs[1].plot(m2, M1, label=r'Equation_1: $\frac{\partial F}{\partial m_1} = 0$', linewidth=2)
-axs[1].plot(M2, m1, label=r'Equation_2: $\frac{\partial F}{\partial m_2} = 0$', linewidth=2)
+axs[1].plot(m1, M2, label=r'Equation_1: $\frac{\partial F}{\partial m_1} = 0$', linewidth=2)
+axs[1].plot(M1, m2, label=r'Equation_2: $\frac{\partial F}{\partial m_2} = 0$', linewidth=2)
 
-axs[1].set_title("$\gamma_1=$" + str(gamma1) + ", $\gamma_2=$" + str(gamma2) + "; $J_{11}=$"+str(J11)+", $J_{22}=$"+str(J22)+", $J_{12}=$"+str(J12)+"; $h_1=$"+str(h1)+", $h_2=$"+str(h2), fontsize=12, y=1.01)
+axs[1].set_title("$\gamma_1=$" + str(gamma1) + ", $\gamma_2=$" + str(gamma2) + "; $J_{11}=$"+str(J11)+", $J_{22}=$"+str(J22)+", $J_{12}=$"+str(J12)+"; $h_1=$"+str(h1)+", $h_2=$"+str(h2), fontsize=12, y=1.01, x=.45)
 axs[1].grid()
 
 plt.tight_layout()
